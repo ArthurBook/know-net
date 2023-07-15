1784c7865467eb583599fb37121c10f19d9d1467
Wian Stipp <stippwian@gmail.com>
2023-07-15 16:08:38 -0500

    added test file for graph buidling

--- know_net/graph_building.py
+++ know_net/graph_building.py
@@ -3,6 +3,7 @@ import networkx as nx
 from pydantic import BaseModel
 from langchain.vectorstores import Chroma
 from langchain.llms import HuggingFaceTextGenInference
+from langchain.indexes import GraphIndexCreator
 from langchain.docstore.document import Document
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
@@ -98,28 +99,3 @@ class LLMGraphBuilder(GraphBuilder):
 
     def search(self, q: str):
         return self.vectorstore.similarity_search(q)
-
-
-T = """"New Breakthrough in Renewable Energy: Solar-Powered Artificial Leaves Set to Revolutionize Clean Energy Production"
-
-Date: July 15, 2023
-
-In a groundbreaking scientific discovery, researchers at a prominent university have developed a revolutionary solar-powered artificial leaf that promises to transform the landscape of renewable energy. This cutting-edge technology aims to provide a sustainable and efficient solution for clean energy production, paving the way towards a greener and more sustainable future.
-
-The research team, led by Dr. Amanda Reynolds, has successfully engineered an artificial leaf capable of mimicking the natural process of photosynthesis. By harnessing sunlight, water, and carbon dioxide, the artificial leaf can generate renewable energy in the form of hydrogen fuel. This breakthrough has the potential to revolutionize the way we produce and utilize clean energy.
-
-Unlike traditional solar panels, which convert sunlight directly into electricity, this new technology directly produces hydrogen gas. The artificial leaf consists of a thin, flexible silicon-based material embedded with specialized catalysts that facilitate the splitting of water molecules into hydrogen and oxygen. The generated hydrogen can then be stored and used as a clean and efficient source of energy.
-"""
-
-if __name__ == "__main__":
-    from langchain.indexes import GraphIndexCreator
-    from langchain.chat_models import ChatOpenAI
-
-    # from langchain.document_loaders import TextLoader
-    # from langchain.chains import GraphQAChain
-
-    builder = LLMGraphBuilder()
-    builder.add_content(T)
-    print(builder.graph)
-    print(builder.triples)
-    print(builder.search("hypoxaemia"))
---
+++ tests/test_graph_building.py
@@ -0,0 +1,19 @@
+from know_net.graph_building import LLMGraphBuilder
+
+T = """"New Breakthrough in Renewable Energy: Solar-Powered Artificial Leaves Set to Revolutionize Clean Energy Production"
+
+Date: July 15, 2023
+
+In a groundbreaking scientific discovery, researchers at a prominent university have developed a revolutionary solar-powered artificial leaf that promises to transform the landscape of renewable energy. This cutting-edge technology aims to provide a sustainable and efficient solution for clean energy production, paving the way towards a greener and more sustainable future.
+
+The research team, led by Dr. Amanda Reynolds, has successfully engineered an artificial leaf capable of mimicking the natural process of photosynthesis. By harnessing sunlight, water, and carbon dioxide, the artificial leaf can generate renewable energy in the form of hydrogen fuel. This breakthrough has the potential to revolutionize the way we produce and utilize clean energy.
+
+Unlike traditional solar panels, which convert sunlight directly into electricity, this new technology directly produces hydrogen gas. The artificial leaf consists of a thin, flexible silicon-based material embedded with specialized catalysts that facilitate the splitting of water molecules into hydrogen and oxygen. The generated hydrogen can then be stored and used as a clean and efficient source of energy.
+"""
+
+if __name__ == "__main__":
+    builder = LLMGraphBuilder()
+    builder.add_content(T)
+    print(builder.graph)
+    print(builder.triples)
+    print(builder.search("hypoxaemia"))
