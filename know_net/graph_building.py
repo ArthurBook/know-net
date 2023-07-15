aba6b4aac1ab5c95d51232937ba80c9a5518cd3f
Wian Stipp <stippwian@gmail.com>
2023-07-15 15:58:54 -0500

    basic entity dedup and search

--- know_net/graph_building.py
+++ know_net/graph_building.py
@@ -1,24 +1,85 @@
-from typing import List, Tuple, NamedTuple
+from typing import List, Tuple, NamedTuple, Optional, Dict
 import networkx as nx
+from pydantic import BaseModel
+from langchain.vectorstores import Chroma
+from langchain.llms import HuggingFaceTextGenInference
+from langchain.docstore.document import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from know_net.base import GraphBuilder
+
+from loguru import logger
+
+
+class Entity(BaseModel):
+    name: str
+    is_a: Optional["Entity"] = None
+
+    def __hash__(self) -> int:
+        return hash(self.name)
 
 
 class KGTriple(NamedTuple):
-    subject: str
+    subject: Entity
     predicate: str
-    object_: str
+    object_: Entity
+
+
+RootEntity = Entity(name="root", is_a=None)
 
 
 class LLMGraphBuilder(GraphBuilder):
     def __init__(self) -> None:
         super().__init__()
-        self.index_creator = GraphIndexCreator(llm=ChatOpenAI(temperature=0))
+        llm = HuggingFaceTextGenInference(
+            inference_server_url="http://100.79.46.78:8081",
+            verbose=True,
+            max_new_tokens=512,
+            top_k=10,
+            top_p=0.95,
+            typical_p=0.95,
+            temperature=0.01,
+        )
+        self.index_creator = GraphIndexCreator(llm=llm)
         self.triples: List[KGTriple] = []
+        self.embeddings = HuggingFaceEmbeddings(model_kwargs={"device": "cuda"})
+        self.vectorstore = Chroma.from_documents([], self.embeddings)
+        self.match_threshold = 0.95
+        self.doc_to_entity: Dict[Document, Entity] = {}
+        logger.info("Initialized LLMGraphBuilder")
 
     def add_content(self, content: str) -> None:
         graph = self.index_creator.from_text(content)
-        triples = [KGTriple(s, p, o) for s, o, p in graph.get_triples()]
-        self.triples.extend(triples)
+        unnormalized_triples: List[Tuple[str, str, str]] = graph.get_triples()
+        normalized_triples: List[KGTriple] = [
+            self._normalize_triple(s, p, o) for s, o, p in unnormalized_triples
+        ]
+        self.triples.extend(normalized_triples)
+
+    def _normalize_triple(self, subject: str, predicate: str, object_: str) -> KGTriple:
+        s = self.vectorstore.similarity_search_with_relevance_scores(subject, k=1)
+        o = self.vectorstore.similarity_search_with_relevance_scores(object_, k=1)
+        if len(s) and s[0][1] > self.match_threshold:
+            logger.debug("match for subject: {}", subject)
+            # if it already exists, then take existing entity
+            s_entity = self.doc_to_entity[s[0][0].page_content]
+        else:
+            logger.debug("miss for subject: {}", subject)
+            # if doesn't exist, add the entity to the vectorebase
+            s_entity = Entity(name=subject)
+            s_doc = Document(page_content=subject)
+            self.vectorstore.add_documents([s_doc])
+            self.doc_to_entity[s_doc.page_content] = s_entity
+        if len(o) and o[0][1] > self.match_threshold:
+            logger.debug("match for object: {}", object_)
+            o_entity = self.doc_to_entity[o[0][0].page_content]
+        else:
+            logger.debug("miss for object: {}", object_)
+            o_entity = Entity(name=object_)
+            o_doc = Document(page_content=object_)
+            self.vectorstore.add_documents([o_doc])
+            self.doc_to_entity[o_doc.page_content] = o_entity
+        return KGTriple(s_entity, predicate, o_entity)
 
     @property
     def graph(self) -> nx.Graph:
@@ -35,16 +96,39 @@ class LLMGraphBuilder(GraphBuilder):
 
         return convert_to_graph(self.triples)
 
+    def search(self, q: str):
+        return self.vectorstore.similarity_search(q)
 
-T = """Epidemiology of hypoxaemia in children with acute lower respiratory infection. To determine the prevalence of hypoxaemia in children aged under 5 years suffering acute lower respiratory infections (ALRI), the risk factors for hypoxaemia in children under 5 years of age with ALRI, and the association of hypoxaemia with an increased risk of dying in children of the same age. Systematic review of the published literature. Out-patient clinics, emergency departments and hospitalisation wards in 23 health centres from 10 countries. Cohort studies reporting the frequency of hypoxaemia in children under 5 years of age with ALRI, and the association between hypoxaemia and the risk of dying. Prevalence of hypoxaemia measured in children with ARI and relative risks for the association between the severity of illness and the frequency of hypoxaemia, and between hypoxaemia and the risk of dying. Seventeen published studies were found that included 4,021 children under 5 with acute respiratory infections (ARI) and reported the prevalence of hypoxaemia. Out-patient children and those with a clinical diagnosis of upper ARI had a low risk of hypoxaemia (pooled estimate of 6% to 9%). The prevalence increased to 31% and to 43% in patients in emergency departments and in cases with clinical pneumonia, respectively, and it was even higher among hospitalised children (47%) and in those with radiographically confirmed pneumonia (72%). The cumulated data also suggest that hypoxaemia is more frequent in children living at high altitude. Three papers reported an association between hypoxaemia and death, with relative risks varying between 1.4 and 4.6. Papers describing predictors of hypoxaemia have focused on clinical signs for detecting hypoxaemia rather than on identifying risk factors for developing this complication. Hypoxaemia is a common and potentially lethal complication of ALRI in children under 5, particularly among those with severe disease and those living at high altitude. Given the observed high prevalence of hypoxaemia and its likely association with increased mortality, efforts should be made to improve the detection of hypoxaemia and to provide oxygen earlier to more children with severe ALRI."""
+
+T = """"New Breakthrough in Renewable Energy: Solar-Powered Artificial Leaves Set to Revolutionize Clean Energy Production"
+
+Date: July 15, 2023
+
+In a groundbreaking scientific discovery, researchers at a prominent university have developed a revolutionary solar-powered artificial leaf that promises to transform the landscape of renewable energy. This cutting-edge technology aims to provide a sustainable and efficient solution for clean energy production, paving the way towards a greener and more sustainable future.
+
+The research team, led by Dr. Amanda Reynolds, has successfully engineered an artificial leaf capable of mimicking the natural process of photosynthesis. By harnessing sunlight, water, and carbon dioxide, the artificial leaf can generate renewable energy in the form of hydrogen fuel. This breakthrough has the potential to revolutionize the way we produce and utilize clean energy.
+
+Unlike traditional solar panels, which convert sunlight directly into electricity, this new technology directly produces hydrogen gas. The artificial leaf consists of a thin, flexible silicon-based material embedded with specialized catalysts that facilitate the splitting of water molecules into hydrogen and oxygen. The generated hydrogen can then be stored and used as a clean and efficient source of energy.
+
+The key advantage of this solar-powered artificial leaf lies in its efficiency. The device can convert sunlight into hydrogen fuel with an impressive efficiency rate of 15%, surpassing the efficiency of most commercially available solar panels. This advancement brings us significantly closer to achieving a cost-effective and sustainable method of producing clean energy on a large scale.
+
+Dr. Reynolds and her team are also working on integrating the artificial leaf into existing renewable energy infrastructure. They envision a future where solar-powered artificial leaves can be easily deployed in large-scale arrays, transforming sunlight and water into hydrogen fuel, which can be utilized for various applications such as powering vehicles, heating buildings, and generating electricity.
+
+The implications of this breakthrough are far-reaching. With the demand for renewable energy on the rise, solar-powered artificial leaves have the potential to meet a substantial portion of global energy needs while reducing carbon emissions. Additionally, this technology holds promise for remote areas and developing countries, providing them with a decentralized and sustainable source of power.
+
+Industry experts and environmentalists alike are hailing this breakthrough as a game-changer in the field of renewable energy. If successfully implemented on a large scale, solar-powered artificial leaves could play a significant role in mitigating climate change and ushering in a cleaner, greener future for generations to come.
+
+As researchers continue to refine and optimize the technology, the prospects of solar-powered artificial leaves are becoming increasingly promising. With continued investment and support, this innovation could soon be a common sight, bringing us closer to achieving a more sustainable and environmentally friendly world."""
 
 if __name__ == "__main__":
     from langchain.indexes import GraphIndexCreator
     from langchain.chat_models import ChatOpenAI
-    from langchain.document_loaders import TextLoader
-    from langchain.chains import GraphQAChain
+
+    # from langchain.document_loaders import TextLoader
+    # from langchain.chains import GraphQAChain
 
     builder = LLMGraphBuilder()
     builder.add_content(T)
     print(builder.graph)
     print(builder.triples)
+    print(builder.search("hypoxaemia"))
