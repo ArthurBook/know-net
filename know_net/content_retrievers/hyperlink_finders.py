d89d41eb65c322de8d7b4ffea1851a15b15df85d
arthur böök <arthur@arthurs-air.basilisk-minor.ts.net>
2023-07-15 20:51:44 -0700

    corrected case for when max links == 0

--- know_net/content_retrievers/hyperlink_finders.py
+++ know_net/content_retrievers/hyperlink_finders.py
@@ -13,7 +13,7 @@ class HyperLinkFinder(abc.ABC):
     """
 
     def __init__(self, max_links: Optional[int] = None) -> None:
-        self.max_links = max_links or DEFAULT_MAX_LINKS
+        self.max_links = max_links if max_links is not None else DEFAULT_MAX_LINKS
 
     def get_links_from_html(self, html: str, base_url: str) -> Iterator[str]:
         soup = bs4.BeautifulSoup(html, "html.parser")
